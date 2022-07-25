0c9814d927aad08ca98478f9521a60a181ce305f
BuildTools <unconfigured@null.spigotmc.org>
2022-07-25 20:13:16 +0500

    start2

--- main5.py
+++ main5.py
@@ -1,5 +1,10 @@
-from kivy.app import App
 from kivy.config import Config
+
+Config.set('graphics', 'resizable', 0)
+Config.set('graphics', 'width', 800)
+Config.set('graphics', 'height', 600)
+
+from kivy.app import App
 from kivy.uix.image import Image
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
@@ -8,11 +13,6 @@ from kivy.uix.button import Button
 from kivy.core.window import Window
 
 
-window_width=640
-window_height=960
-Config.set('graphics', 'resizable', 0)
-Config.set('graphics', 'width', window_width)
-Config.set('graphics', 'height', window_height)
 
 
 class ImgShow(Widget):
@@ -21,8 +21,8 @@ class ImgShow(Widget):
             print('down')
 
     def on_touch_move(self, touch):
-        print('move')
-        self.width += .1
+        print('move', self.img)
+
 
 class MyApp(App):
     def build(self):
@@ -30,11 +30,15 @@ class MyApp(App):
         parent = Widget()
         self.dadada = ImgShow()
         parent.add_widget(self.dadada)
-        gl = GridLayout(rows=2, spacing=3, size_hint=(None, None), size=[window_width, window_height])
-        bl_up = BoxLayout(orientation='vertical')
+        gl = GridLayout(rows=2, spacing=3, size_hint=(None, None), size=[800, 600])
+        bl_up = BoxLayout(orientation='vertical', size_hint=[None, None], size=[300, 50])
+        bl_up.clearcolor = (1, 1, 1, 1)
         bl_up.add_widget(Button(text='dada', on_press=self.click_button))
         bl_down = BoxLayout(orientation='vertical')
-        bl_down.add_widget(Button(text='dad2a', on_press=self.click_button))
+        #bl_down.add_widget(Button(text='dad2a', on_press=self.click_button))
+        self.img = Image(source='mytopkid-labirint-4-5-38.jpg', size_hint=(1, .9), allow_stretch=True,
+                         pos_hint={'center_x': .5, 'center_y': .5})
+        bl_down.add_widget(self.img)
         gl.add_widget(bl_up)
         gl.add_widget(bl_down)
         parent.add_widget(gl)
--- test.py
+++ test.py
@@ -1,30 +1,58 @@
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.properties import ColorProperty
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
-
-Builder.load_string(
-"""
-
-<CustomRoot>:
-    canvas:
-        Color:
-            rgba: root.background_color
-        Rectangle:
-            pos: self.pos
-            size: self.size
-
-""")
-
-
-class CustomRoot(BoxLayout):
-    background_color = ColorProperty()  # The ListProperty will also work.
-
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
 
 class MyApp(App):
 
+    main_layout = BoxLayout(orientation='vertical')
+    top_layout = BoxLayout(orientation='horizontal')
+
+    scrollView = ScrollView()
+    gridLayout = GridLayout(size_hint_y=None)
+
+    gridLayout.cols = 1
+    gridLayout.padding = [0, 0, 0, 0]
+    gridLayout.bind(minimum_height=gridLayout.setter('height'))
+    scrollView.add_widget(gridLayout)
+
+    main_layout.add_widget(top_layout)
+    main_layout.add_widget(scrollView)
+
+    def btn_create(self, instance):
+        self.gridLayout.add_widget(Label(text='test', size_hint_y=None))
+
+    def btn_edit(self, instance):
+        pass
+
+    def btn_delete(self, instance):
+        pass
+
     def build(self):
-        return CustomRoot(background_color=(1, 0, 1, 1))
+        self.top_layout.size_hint=(1, .1)
 
+        # Button 'Erstellen'
+        btnCreate = Button()
+        btnCreate.text = 'Erstellen'
+        btnCreate.bind(on_press=self.btn_create)
 
-MyApp().run()
+        # Button 'Bearbeiten'
+        btnEdit = Button()
+        btnEdit.text = 'Bearbeiten'
+        btnEdit.bind(on_press=self.btn_edit)
+
+        # Button 'Löschen'
+        btnDelete = Button()
+        btnDelete.text = 'Löschen'
+        btnDelete.bind(on_press=self.btn_delete)
+
+        self.top_layout.add_widget(btnCreate)
+        self.top_layout.add_widget(btnEdit)
+        self.top_layout.add_widget(btnDelete)
+
+        return self.main_layout
+
+if __name__ == '__main__':
+    MyApp().run()
