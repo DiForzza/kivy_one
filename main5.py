b20254b381a3bba23ea1162966572ccb49e0b325
BuildTools <unconfigured@null.spigotmc.org>
2022-07-25 21:28:43 +0500

    start3

--- main5.py
+++ main5.py
@@ -11,8 +11,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.core.window import Window
-
-
+from kivy.uix.scatter import Scatter
 
 
 class ImgShow(Widget):
@@ -21,12 +20,12 @@ class ImgShow(Widget):
             print('down')
 
     def on_touch_move(self, touch):
-        print('move', self.img)
+        print('move', touch)
 
 
 class MyApp(App):
     def build(self):
-        #Window.clearcolor = (1, 1, 1, 1)
+        # Window.clearcolor = (1, 1, 1, 1)
         parent = Widget()
         self.dadada = ImgShow()
         parent.add_widget(self.dadada)
@@ -35,15 +34,15 @@ class MyApp(App):
         bl_up.clearcolor = (1, 1, 1, 1)
         bl_up.add_widget(Button(text='dada', on_press=self.click_button))
         bl_down = BoxLayout(orientation='vertical')
-        #bl_down.add_widget(Button(text='dad2a', on_press=self.click_button))
-        self.img = Image(source='mytopkid-labirint-4-5-38.jpg', size_hint=(1, .9), allow_stretch=True,
+        scatter = Scatter(do_rotation=False)
+        img = Image(source='mytopkid-labirint-4-5-38.jpg', size_hint=(1, .9), allow_stretch=False,
                          pos_hint={'center_x': .5, 'center_y': .5})
-        bl_down.add_widget(self.img)
+        scatter.add_widget(img)
+        bl_down.add_widget(scatter)
         gl.add_widget(bl_up)
         gl.add_widget(bl_down)
         parent.add_widget(gl)
         return parent
-
 
     def click_button(self, i):
         print('dada')
@@ -52,6 +51,5 @@ class MyApp(App):
 if __name__ == '__main__':
     MyApp().run()
 
-
-  #      self.img = Image(source='mytopkid-labirint-4-5-38.jpg', size_hint=(1, .5),
-                         # pos_hint={'center_x': .5, 'center_y': .5})
+#      self.img = Image(source='mytopkid-labirint-4-5-38.jpg', size_hint=(1, .5),
+# pos_hint={'center_x': .5, 'center_y': .5})
