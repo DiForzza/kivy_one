eacd774e6092d841c4a8b6e325be16f9014127c8
BuildTools <unconfigured@null.spigotmc.org>
2022-07-26 21:23:20 +0500

    start4

--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
+from kivy.properties import ObjectProperty
 
 
 class Container(GridLayout):
+    text_input = ObjectProperty()
+    label_widget = ObjectProperty()
+
     def change_text(self):
-        self.label_widget.text = self.chtonibub.text
+        self.label_widget.text = self.text_input.text
 
 
 class MyApp(App):
