2c4a9fce268f376fcc65235046ec2cfa2a337757
BuildTools <unconfigured@null.spigotmc.org>
2022-08-05 23:38:36 +0500

    Course

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
@@ -6,8 +7,17 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
 
 
-#class GridLayoutExample(GridLayout):
-#    pass
+class WidgetsExample(GridLayout):
+    count = 1
+    my_text = StringProperty(str(count))
+
+    def on_button_click(self):
+        print(self.count)
+        self.count += 1
+        self.my_text = str(self.count)
+
+    def on_toggle_button_state(self, state):
+        print(state.state)
 
 
 class AnchorLayoutExample(AnchorLayout):
@@ -16,6 +26,8 @@ class AnchorLayoutExample(AnchorLayout):
 
 class BoxLayoutExample(BoxLayout):
     pass
+
+
 """    def __init__(self, **kwargs):
         super().__init__(**kwargs)
         b1 = Button(text='A')
