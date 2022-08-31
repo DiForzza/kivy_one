72f1757d95a247dd8eff2210f1e40ff2c096306d
BuildTools <unconfigured@null.spigotmc.org>
2022-08-31 21:01:55 +0500

    added all 4

--- Old14.08.2022/multiplication4.py
+++ Old14.08.2022/multiplication4.py
@@ -1,6 +1,11 @@
-import random
-
 from kivy.config import Config
+from kivymd.uix.boxlayout import MDBoxLayout
+
+Config.set('graphics', 'resizable', 0)
+Config.set('graphics', 'width', 400)
+Config.set('graphics', 'height', 800)
+
+import random
 from kivy.lang import Builder
 from kivy.properties import ListProperty
 from kivy.uix.switch import Switch
@@ -9,29 +14,31 @@ from kivy.uix.button import Button
 from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.label import MDLabel
 
-Config.set('graphics', 'resizable', 0)
-Config.set('graphics', 'width', 200)
-Config.set('graphics', 'height', 500)
+
 
 KV = """
-MDBoxLayout:
-    orientation: 'vertical'
-    MDBoxLayout:
-        md_bg_color: app.theme_cls.primary_light
-        # MDFlatButton:
-        #     size_hint: 1., 1.
-        #     text: "Button"
-        # MDFlatButton:
-        #     size_hint: 1., 1.
-        #     text: "Button"
-    MDFlatButton:
-        size_hint: 1., 1.
-        text: "Button"
-    MDFlatButton:
-        size_hint: 1., 1.
-        text: "Button"
+MDScreen:
+    MDNavigationLayout:
+        MDScreenManager:
+            MDScreen:
+                MDTopAppBar:
+                    title: "Navigation Drawer"
+                    elevation: 4
+                    pos_hint: {"top": 1}
+                    md_bg_color: "#e7e4c0"
+                    specific_text_color: "#4a4939"
+                    left_action_items:
+                        [['menu', lambda x: nav_drawer.set_state("open")]]
+        MDNavigationDrawer:
+            id: nav_drawer
+            radius: (0, 16, 16, 0)
+        
+            ContentNavigationDrawer:
+                MDFlatButton:
 """
 
+class ContentNavigationDrawer(MDBoxLayout):
+    pass
 
 class MultiApp(MDApp):
 
