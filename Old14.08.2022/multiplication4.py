5dfc3e782cc409c09e0a8f83766c12359b2afdf8
DiForzza <uit@pm.convex.ru>
2022-08-31 13:02:01 +0500

    mult4 start 2

--- Old14.08.2022/multiplication4.py
+++ Old14.08.2022/multiplication4.py
@@ -1,4 +1,6 @@
 import random
+
+from kivy.config import Config
 from kivy.lang import Builder
 from kivy.properties import ListProperty
 from kivy.uix.switch import Switch
@@ -7,17 +9,26 @@ from kivy.uix.button import Button
 from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.label import MDLabel
 
+Config.set('graphics', 'resizable', 0)
+Config.set('graphics', 'width', 200)
+Config.set('graphics', 'height', 500)
+
 KV = """
 MDBoxLayout:
-    MDGridLayout:
-        cols: 2
-        MDFlatButton:
-            text: "Button"
-        MDFlatButton:
-            text: "Button"
+    orientation: 'vertical'
+    MDBoxLayout:
+        md_bg_color: app.theme_cls.primary_light
+        # MDFlatButton:
+        #     size_hint: 1., 1.
+        #     text: "Button"
+        # MDFlatButton:
+        #     size_hint: 1., 1.
+        #     text: "Button"
     MDFlatButton:
+        size_hint: 1., 1.
         text: "Button"
     MDFlatButton:
+        size_hint: 1., 1.
         text: "Button"
 """
 
@@ -25,6 +36,8 @@ MDBoxLayout:
 class MultiApp(MDApp):
 
     def build(self):
+        self.theme_cls.primary_palette = "Orange"
+        self.theme_cls.theme_style = "Light"
         return Builder.load_string(KV)
 
 
