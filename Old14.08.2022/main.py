4694ab33fcd4c490d3edc7388088f5b5e8729bc2
DiForzza <uit@pm.convex.ru>
2022-09-02 12:59:54 +0500

    Kivy Course - Create Python Games and Mobile Apps 2:39:31

--- Old14.08.2022/main.py
+++ Old14.08.2022/main.py
@@ -1,7 +1,7 @@
 from kivy.app import App
-from kivy.graphics import Line
+from kivy.graphics import Line, Ellipse
 from kivy.metrics import dp
-from kivy.properties import StringProperty, BooleanProperty
+from kivy.properties import StringProperty, BooleanProperty, Clock
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
@@ -96,12 +96,57 @@ class CanvasExample4(Widget):
             self.line = Line(points=(100, 100, 300, 400))
             Line(circle=(400, 100, 300), width=2)
 
+
+class CanvasExample5(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ball_size = dp(50)
+        self.vx = dp(10)
+        self.vy = dp(10)
+        with self.canvas:
+            self.ball = Ellipse(pos=(100,100), size=(self.ball_size, self.ball_size))
+        Clock.schedule_interval(self.update, 1/60)
+
+   # def on_size(self, *args):
+        #print(str(self.width), str(self.height))
+        #self.ball.pos = (self.center_x-self.ball_size/2, self.center_y-self.ball_size/2)
+
+    def update(self, dt):
+        #print('updated')
+        x, y = self.ball.pos
+
+        x += self.vx
+        y += self.vy
+
+        if y + self.ball_size > self.height:
+            y = self.height - self.ball_size
+            self.vy = - self.vy
+        if x + self.ball_size > self.width:
+            x = self.width - self.ball_size
+            self.vx = - self.vx
+        if y < 0:
+            y = 0
+            self.vy = -self.vy
+        if x < 0:
+            x = 0
+            self.vx = -self.vx
+
+        self.ball.pos = (x, y)
+
     def on_button_a_click(self):
         x1, y1, x2, y2 = self.line.points
         x2 += dp(50)
         if x2 > self.width:
             x2 = 0
         self.line.points = (300, 100, x2, 400)
+
+
+class CanvasExample6(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class CanvasExample7(BoxLayout):
+    pass
 
 if __name__ == "__main__":
     app = TheLabApp()
--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-multiplication_number = []
-for i in range(1, 10):
-    for j in range(1, 10):
-        print(i, j)
-        multiplication_number.append(i * j)
+from kivy.app import App
+from kivy.uix.widget import Widget
 
-multiplication_number = set(multiplication_number)
-print(multiplication_number)
 
-#1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 24, 25, 27, 28, 30, 32, 35, 36, 40, 42, 45, 48, 49, 54, 56, 63, 64, 72, 81
+class MainWidget(Widget):
+    pass
 
-# 1, 2, 3, 4, 5, 6, 7, 8, 9
-# 10, 12, 14, 15, 16, 18
-# 20, 21, 24, 25, 27, 28,
-# 30, 32, 35, 36
-# 40, 42, 45, 48, 49
-# 54, 56
-# 63, 64
-# 72
-# 81
+
+class GalaxyApp(App):
+    pass
+
+
+GalaxyApp().run()
