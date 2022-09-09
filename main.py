81d32a76590dba9a215cb2bca751a7934e637d12
DiForzza <uit@pm.convex.ru>
2022-09-09 12:55:58 +0500

    Kivy Course - Create Python Games and Mobile Apps 3:50:53

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from kivy.config import Config
+from kivy.core.window import Window
+
+Config.set('graphics', 'width', '900')
+Config.set('graphics', 'height', '400')
 from kivy.app import App
 from kivy.graphics import Color, Line
 from kivy.properties import NumericProperty, Clock
@@ -9,83 +14,82 @@ class MainWidget(Widget):
     perspective_point_y = NumericProperty(0)
 
     V_NB_LINES = 10
-    V_LINES_SPACING = .25 #percentage in screen wight
+    V_LINES_SPACING = .25  # percentage in screen wight
     vertical_lines = []
 
     H_NB_LINES = 8
-    H_LINES_SPACING = .1 #percentage in screen wight
+    H_LINES_SPACING = .1  # percentage in screen wight
     horizontal_lines = []
 
-    SPEED = 4
-
+    SPEED = 2
     current_offset_y = 0
+
+    SPEED_X = 12
+    current_speed_x = 0
+    current_offset_x = 0
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
         self.init_vertical_lines()
         self.init_horizontal_lines()
+        self._keyboard = Window.request_keyboard(self.keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self.on_keyboard_down)
+        self._keyboard.bind(on_key_up=self.on_keyboard_up)
         Clock.schedule_interval(self.update, 1.0 / 60.0)
+
+    def keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self.on_keyboard_down)
+        self._keyboard.unbind(on_key_up=self.on_keyboard_up)
+        self._keyboard = None
 
     def on_parent(self, widget, parent):
         pass
 
     def on_size(self, *args):
-        #print(str(self.width), str(self.height))
-        #self.perspective_point_x = self.width/2
-        #self.perspective_point_y = self.height * 0.75
         pass
 
     def on_perspective_point_x(self, widget, value):
-        #print(str(value))
         pass
 
     def on_perspective_point_y(self, widget, value):
-        #print(str(value))
         pass
 
     def init_vertical_lines(self):
         with self.canvas:
             Color(1, 1, 1)
-            #self.line = Line(points=[100, 0, 100, 100])
             for i in range(0, self.V_NB_LINES):
                 self.vertical_lines.append(Line())
 
     def init_horizontal_lines(self):
         with self.canvas:
             Color(1, 1, 1)
-            #self.line = Line(points=[100, 0, 100, 100])
             for i in range(0, self.H_NB_LINES):
                 self.horizontal_lines.append(Line())
 
     def update_vertical_lines(self):
-        central_line_x = int(self.width/2)
+        central_line_x = int(self.width / 2)
         spacing = self.V_LINES_SPACING * self.width
-        offset = -int(self.V_NB_LINES/2) + 0.5
+        offset = -int(self.V_NB_LINES / 2) + 0.5
         for i in range(0, self.V_NB_LINES):
-            line_x = int(central_line_x + offset * spacing)
-
+            line_x = central_line_x + offset * spacing + self.current_offset_x
             x1, y1 = self.transform(line_x, 0)
             x2, y2 = self.transform(line_x, self.height)
             self.vertical_lines[i].points = [x1, y1, x2, y2]
             offset += 1
 
     def update_horizontal_lines(self):
-        central_line_x = int(self.width/2)
+        central_line_x = int(self.width / 2)
         spacing = self.V_LINES_SPACING * self.width
-        offset = -int(self.V_NB_LINES/2) + 0.5
-
-        xmin = central_line_x + offset * spacing
-        xmax = central_line_x - offset * spacing
+        offset = -int(self.V_NB_LINES / 2) + 0.5
+        xmin = central_line_x + offset * spacing + self.current_offset_x
+        xmax = central_line_x - offset * spacing + self.current_offset_x
         spacing_y = self.H_LINES_SPACING * self.height
 
-
         for i in range(0, self.H_NB_LINES):
-            line_y = i * spacing_y -self.current_offset_y
-
+            line_y = i * spacing_y - self.current_offset_y
             x1, y1 = self.transform(xmin, line_y)
             x2, y2 = self.transform(xmax, line_y)
             self.horizontal_lines[i].points = [x1, y1, x2, y2]
-
 
     def transform(self, x, y):
         # return self.transform_2D(x, y)
@@ -109,15 +113,38 @@ class MainWidget(Widget):
 
         return int(tr_x), int(tr_y)
 
+    def on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if keycode[1] == 'left':
+            self.current_speed_x = self.SPEED_X
+        elif keycode[1] == 'right':
+            self.current_speed_x = -self.SPEED_X
+        return True
+
+    def on_keyboard_up(self, keyboard, keycode):
+        self.current_speed_x = 0
+
+    def on_touch_down(self, touch):
+        if touch.x < self.width/2:
+            self.current_speed_x = self.SPEED_X
+        else:
+            self.current_speed_x = -self.SPEED_X
+
+    def on_touch_up(self, touch):
+        self.current_speed_x = 0
+
     def update(self, dt):
         # print('dt', dt)
+        time_factor = dt * 60
         self.update_vertical_lines()
         self.update_horizontal_lines()
-        self.current_offset_y += self.SPEED
+        self.current_offset_y += self.SPEED * time_factor
 
         spacing_y = self.H_LINES_SPACING * self.height
         if self.current_offset_y >= spacing_y:
             self.current_offset_y -= spacing_y
+
+        self.current_offset_x += self.current_speed_x * time_factor
+
 
 class GalaxyApp(App):
     pass
